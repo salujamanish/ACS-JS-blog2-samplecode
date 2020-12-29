e0a816c37e8fc8cbec4541005e6783da9e34a4ad
salujamanish <56380683+salujamanish@users.noreply.github.com>
2020-12-29 12:54:31 +0530

    code formating

--- user-mgt/user-mgt.js
+++ user-mgt/user-mgt.js
@@ -1,26 +1,37 @@
-const { CommunicationIdentityClient } = require('@azure/communication-administration');
+const {
+  CommunicationIdentityClient,
+} = require("@azure/communication-administration");
 
 const main = async () => {
-    console.log("Azure Communication Services - Access Tokens Quickstart")
+  console.log("Azure Communication Services - Access Tokens Quickstart");
 
-    // This code demonstrates how to fetch your connection string
-    // from an environment variable.
-    const connectionString = process.env['COMMUNICATION_SERVICES_CONNECTION_STRING'];
+  // This code demonstrates how to fetch your connection string
+  // from an environment variable.
+  const connectionString =
+    process.env["COMMUNICATION_SERVICES_CONNECTION_STRING"];
 
-    // Instantiate the identity client
-    const identityClient = new CommunicationIdentityClient(connectionString);
+  // Instantiate the identity client
+  const identityClient = new CommunicationIdentityClient(connectionString);
 
-    let identityResponse = await identityClient.createUser();
-    console.log('\nCreated an identity with ID: ', identityResponse.communicationUserId);
+  let identityResponse = await identityClient.createUser();
+  console.log(
+    "\nCreated an identity with ID: ",
+    identityResponse.communicationUserId
+  );
 
-    // Issue an access token with the "chat" scope for an identity
-    let tokenResponse = await identityClient.issueToken(identityResponse, ["chat"]);
-    const { token, expiresOn } = tokenResponse;
-    console.log('\nIssued an access token with \'chat\' scope that expires at', expiresOn);
-    console.log(token);
+  // Issue an access token with the "chat" scope for an identity
+  let tokenResponse = await identityClient.issueToken(identityResponse, [
+    "chat",
+  ]);
+  const { token, expiresOn } = tokenResponse;
+  console.log(
+    "\nIssued an access token with 'chat' scope that expires at",
+    expiresOn
+  );
+  console.log(token);
 };
 
 main().catch((error) => {
   console.log("Encountered an error");
   console.log(error);
-})
+});
